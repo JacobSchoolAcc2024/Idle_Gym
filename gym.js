ab3a44c00676c0473ef173898127b317c0c678ae
JacobSchoolAcc2024 <21btorcino@wakatipu.school.nz>
2024-05-02 10:22:08 +1200

    Added more functionality

--- gym.js
+++ gym.js
@@ -1,4 +1,4 @@
-let str = 0;
+let str = 1
 let strGain = 1
 let energy = 5;
 let injury = 0;
@@ -8,10 +8,36 @@ let energyRecoveryInterval = 2000;
 
 
 function sit_up(){
-
-
+    if (weights >= 5){
+        if (energy >= 2.5 && injury < 10) {
+            str += strGain + 1;
+            energy -= 2.5;
+            document.getElementById("str").innerText = "Strength: " + str;
+            document.getElementById("energy").innerText = "Energy: " + energy;
+        } else {
+            injury += 1;
+            document.getElementById("injury").innerText = "Injury: " + injury;
+            if (injury >= 10) {
+                alert("Injury reached 10. You lose all your strenght!");
+                injury = 0;
+                str = 0;
+                energy = 0; // Reset energy to 0
+                energyRecoveryInterval = 5000; // Increase energy recovery interval
+                document.getElementById("str").innerText = "Strength: " + str;
+                document.getElementById("injury").innerText = "Injury: " + injury;
+            }
+        }
+        checkStr();
+    }
 }
 
+function check_sit_up(){
+    if (weights >= 5) {
+        document.getElementById("wide_pushups").style.display = "block";
+        return true
+    }
+    
+}
 
 
 
@@ -48,16 +74,19 @@ function updateStr() {
 }
 
 
-
-
 function weight(){
     if(str>= 10){
+      weights += 1;
       str -= 10; 
       strGain += 0.5;
       
       document.getElementById("str").innerText = "Strength: " + str;
+      document.getElementById("weights_owned").innerText = "Weights owned: " + weights;
       
-      checkStr(); // add this
+      checkStr();
+      if (check_sit_up() === true) {
+        sit_up()
+      }
     }
   } 
   
@@ -80,6 +109,8 @@ function injuryRecovery() {
         document.getElementById("injury").innerText = "Injury: " + injury;
     }
 }
+
+
 
 // Set intervals for energy and injury recovery
 setInterval(regainEnergy, energyRecoveryInterval);
